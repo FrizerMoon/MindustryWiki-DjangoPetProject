9985c3ae09707f7ec3336a0c6ff8e93bd7dc4785
Frizer_moon <killerskill14play@gmail.com>
2026-08-27 20:16:15 +0300

    change main page,adding sign up button

--- Django/urls.py
+++ Django/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('users/',include('users.urls')),
     path('admin/', admin.site.urls),
-    path('',include('main.urls'))
+    path('',include('main.urls')),
 ]
